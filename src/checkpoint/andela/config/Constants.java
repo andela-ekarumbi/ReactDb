4b97e375984c97390fc300351a2533bb74068906
Eston Karumbi <eston.karumbi@andela.com>
2016-03-04 23:39:57 +0300

    Create Constants class

---
+++ src/checkpoint/andela/config/Constants.java
@@ -0,0 +1,17 @@
+package checkpoint.andela.config;
+
+public class Constants {
+    public static final String[] ALLOWED_ATTRIBUTES = {
+            "UNIQUE-ID",
+            "TYPES",
+            "ATOM-MAPPINGS",
+            "CREDITS",
+            "EC-NUMBER",
+            "ENZYMATIC-REACTION",
+            "LEFT",
+            "ORPHAN",
+            "PHYSIOLOGICALLY-RELEVANT",
+            "REACTION-DIRECTION",
+            "RIGHT"
+    };
+}
