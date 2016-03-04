17ca2f86660daf21d3e1255384aa3ff80c69683c
Eston Karumbi <eston.karumbi@andela.com>
2016-03-04 23:36:53 +0300

    Create class DbRecord

---
+++ src/checkpoint/andela/utility/DbRecord.java
@@ -0,0 +1,32 @@
+package checkpoint.andela.utility;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DbRecord {
+
+    private Map<String, List<String>> columns;
+
+    public DbRecord() {
+        this.columns = new HashMap<>();
+    }
+
+    public boolean addColumn(String columnName, String columnValue) {
+        List<String> existingEntries = columns.get(columnName);
+
+        if (existingEntries == null) {
+            existingEntries = new ArrayList<>();
+        }
+
+        existingEntries.add(columnValue);
+        columns.put(columnName, existingEntries);
+
+        return true;
+    }
+
+    public Map<String, List<String>> getAllColumns() {
+        return columns;
+    }
+}
