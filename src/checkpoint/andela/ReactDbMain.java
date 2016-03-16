e1e68398d25b5b53bc9bff27887eddc9ad2767bf
Eston Karumbi <eston.karumbi@andela.com>
2016-03-16 21:58:39 +0300

    Create main class

--- src/checkpoint/andela/ReactDbMain.java
+++ src/checkpoint/andela/ReactDbMain.java
@@ -5,21 +5,28 @@ import checkpoint.andela.buffer.BufferSingletons;
 import checkpoint.andela.config.Constants;
 import checkpoint.andela.db.DBWriter;
 import checkpoint.andela.db.DbHelper;
-import checkpoint.andela.db.DbRecord;
 import checkpoint.andela.log.LogWriter;
+import checkpoint.andela.models.Reaction;
 import checkpoint.andela.parser.FileParser;
+
+import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class ReactDbMain {
 
     public static void main(String[] args) {
+        String logFileName = "logs/logFile "
+                + (new Date()).toString()
+                + ".txt";
 
-
-        /*Buffer<DbRecord> dbRecordBuffer = BufferSingletons.getReactionBuffer();
+        Buffer<Reaction> reactionBuffer = BufferSingletons.getReactionBuffer();
         Buffer<String> logBuffer = BufferSingletons.getStringLogBuffer();
 
 
         FileParser fileParser = new FileParser("data/reactions.dat",
-                dbRecordBuffer,
+                reactionBuffer,
                 logBuffer);
 
         DbHelper dbHelper = new DbHelper(Constants.MYSQL_DRIVER_NAME,
@@ -29,16 +36,21 @@ public class ReactDbMain {
                 Constants.DATABASE_NAME,
                 Constants.MYSQL_TABLE_NAME);
 
-        DBWriter dbWriter = new DBWriter(dbRecordBuffer, dbHelper, logBuffer);
+        DBWriter dbWriter = new DBWriter(reactionBuffer, dbHelper, logBuffer);
 
-        LogWriter logWriter = new LogWriter(logBuffer, "logs/log.txt");
+        LogWriter logWriter = new LogWriter(logBuffer, logFileName);
 
         Thread fileParserThread = new Thread(fileParser);
-        Thread dbWriterThread = new Thread(dbWriter);
-        Thread logWriterThread = new Thread(logWriter);
+
+        ScheduledExecutorService scheduledExecutorService
+                = Executors.newScheduledThreadPool(2);
+
+        scheduledExecutorService
+                .scheduleAtFixedRate(dbWriter, 0, 10, TimeUnit.MILLISECONDS);
+
+        scheduledExecutorService
+                .scheduleAtFixedRate(logWriter, 0, 10, TimeUnit.MILLISECONDS);
 
         fileParserThread.start();
-        dbWriterThread.start();
-        logWriterThread.start();*/
     }
 }
