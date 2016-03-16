4d5a3398ecfca398190a6f36ec57b7fe88800973
Eston Karumbi <eston.karumbi@andela.com>
2016-03-16 08:58:38 +0300

    Refactor Constants

--- src/checkpoint/andela/config/Constants.java
+++ src/checkpoint/andela/config/Constants.java
@@ -2,20 +2,6 @@ package checkpoint.andela.config;
 
 public class Constants {
 
-    public static final String[] ALLOWED_ATTRIBUTES = {
-            "UNIQUE-ID",
-            "TYPES",
-            "ATOM-MAPPINGS",
-            "CREDITS",
-            "EC-NUMBER",
-            "ENZYMATIC-REACTION",
-            "LEFT",
-            "ORPHAN",
-            "PHYSIOLOGICALLY-RELEVANT",
-            "REACTION-DIRECTION",
-            "RIGHT"
-    };
-
     public static final String MYSQL_URL
             = "jdbc:mysql://localhost/reactiondb";
 
@@ -26,4 +12,6 @@ public class Constants {
     public static final String MYSQL_DRIVER_NAME = "com.mysql.jdbc.Driver";
 
     public static final String MYSQL_TABLE_NAME = "reactions";
+
+    public static final String DATABASE_NAME = "reactiondb";
 }
