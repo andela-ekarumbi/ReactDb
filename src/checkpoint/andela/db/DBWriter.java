8ad9f47fc96248915ca4e7cdbdcc14774492e0bd
Eston Karumbi <eston.karumbi@andela.com>
2016-03-16 22:12:14 +0300

    Refactor DBWriter

--- src/checkpoint/andela/db/DBWriter.java
+++ src/checkpoint/andela/db/DBWriter.java
@@ -12,66 +12,46 @@ import checkpoint.andela.utility.Utility;
 
 import java.util.List;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
 public class DBWriter implements Runnable {
-
-    private ScheduledWrite scheduledWrite;
 
     private String bufferTrackingKey;
 
+    private Buffer<Reaction> reactionBuffer;
+
+    private Buffer<String> logBuffer;
+
+    private DbHelper dbHelper;
+
+    List<Reaction> latestReactions;
+
     /**
      * Creates a new {@code DBWriter}.
+     *
      * @param reactionBuffer the reactionBuffer containing the records to be written to the
      * database.
-     * @param writer the object containing the logic for persisting data to
+     * @param dbHelper the object containing the logic for persisting data to
      * the database.
      * @param logBuffer the reactionBuffer containing log messages.
-     * */
+     */
 
     public DBWriter(Buffer<Reaction> reactionBuffer,
-                    DbHelper writer,
+                    DbHelper dbHelper,
                     Buffer<String> logBuffer) {
 
-        scheduledWrite = new ScheduledWrite(reactionBuffer, writer, logBuffer);
+        this.reactionBuffer = reactionBuffer;
+        this.dbHelper = dbHelper;
+        this.logBuffer = logBuffer;
 
         bufferTrackingKey = reactionBuffer.registerClientForTracking();
     }
 
     @Override
     public void run() {
-        Timer timer = new Timer();
-        timer.schedule(scheduledWrite, 0, 10);
+        startWritingData();
     }
 
-    /**
-     * This private class packages the logic for class DBWriter as an operation
-     * that may be called repeatedly at specified time intervals.
-     * */
-
-    private class ScheduledWrite extends TimerTask {
-        private Buffer<Reaction> reactionBuffer;
-        private Buffer<String> logBuffer;
-
-        private DbHelper dbHelper;
-
-        List<Reaction> latestReactions;
-
-        public ScheduledWrite(Buffer<Reaction> reactionBuffer,
-                              DbHelper dbHelper,
-                              Buffer<String> logBuffer) {
-            this.reactionBuffer = reactionBuffer;
-            this.dbHelper = dbHelper;
-            this.logBuffer = logBuffer;
-        }
-
-        @Override
-        public void run() {
-            startWritingData();
-        }
-
-        private void startWritingData() {
+    private void startWritingData() {
+        if (reactionBuffer.isThereNewData(bufferTrackingKey)) {
             try {
                 getReactionsFromBuffer();
                 if (latestReactions != null && latestReactions.size() > 0) {
@@ -81,28 +61,26 @@ public class DBWriter implements Runnable {
                 exception.printStackTrace();
             }
         }
+    }
 
-        private void getReactionsFromBuffer() {
-            if (reactionBuffer.isThereNewData(bufferTrackingKey)) {
-                latestReactions = reactionBuffer.getLatestData(bufferTrackingKey);
+    private void getReactionsFromBuffer() {
+        latestReactions = reactionBuffer.getLatestData(bufferTrackingKey);
+    }
+
+    private void writeReactionsToDatabase() {
+        if (latestReactions != null) {
+            for (Reaction reaction : latestReactions) {
+                writeLog(reaction);
             }
+            dbHelper.writeReactions(latestReactions);
         }
+    }
 
-        private void writeReactionsToDatabase() {
-            if (latestReactions != null) {
-                dbHelper.writeReactions(latestReactions);
-                for (Reaction reaction : latestReactions) {
-                    writeLog(reaction);
-                }
-            }
-        }
-
-        private void writeLog(Reaction reaction) {
-            String messageTermination = " from buffer and wrote to database.";
-            String logMessage
-                    = Utility.generateLogMessage(reaction, messageTermination,
-                    "DBWriter Thread");
-            logBuffer.addToBuffer(logMessage);
-        }
+    private void writeLog(Reaction reaction) {
+        String messageTermination = " from buffer and wrote to database.";
+        String logMessage
+                = Utility.generateLogMessage(reaction, messageTermination,
+                "DBWriter Thread");
+        logBuffer.addToBuffer(logMessage);
     }
 }
